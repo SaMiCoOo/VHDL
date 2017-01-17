8fca1c12788f1099a433ae6138b50b74f0754db5
Abdullah Tayel <atayel@tayel.com>
2017-01-17 17:06:53 +0200

    Register Done

---
+++ Reg.vhd
@@ -0,0 +1,27 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity Reg is
+	port(
+		clk:	in std_logic;
+		D:	in std_logic_vector(31 downto 0);
+		Q:	out std_logic_vector(31 downto 0);
+		nQ:	out std_logic_vector(31 downto 0)
+	);
+end Reg;
+
+architecture behavior of Reg is
+	component DFF
+	port(
+		clk:	in std_logic;
+		D:	in std_logic;
+		Q:	out std_logic;
+		nQ:	out std_logic	
+	);
+	end component;
+begin
+
+	X: for i in 0 to 31 generate
+		DFFX: DFF port map(clk,D(i),Q(i),nQ(i));
+	end generate X;
+end behavior;
